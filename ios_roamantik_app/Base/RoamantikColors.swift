81a2f42afc2518fc667b21330f0929c72b5e9fd4
scayetanot <sebastien.cayetanot@icloud.com>
2023-09-03 23:24:45 -0700

    update

---
+++ ios_roamantik_app/Base/RoamantikColors.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  ios_roamantik_app
+//
+//  Created by Sebastien Cayetanot on 9/2/23.
+//
+
+import SwiftUI
+
+struct RoamantikColors {
+    static let cDeepBlue = Color("DeepBlue")
+    static let cSoftBeige = Color("SoftBeige")
+}
